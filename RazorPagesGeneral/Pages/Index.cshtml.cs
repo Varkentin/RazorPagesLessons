00d4953f0e091545f2469ac6666704d8e526777d
Vark <wardi@mail.ru>
2020-10-04 16:51:46 +0700

    stat!

---
+++ RazorPagesGeneral/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace RazorPagesGeneral.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Message { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Message = $"Welcome to our first Razor Page APP\nThe time now is:{DateTime.Now.ToLongTimeString()}";
+        }
+    }
+}
