5be01260513dbeb2234c01ef2cc84bf13a2f9e8c
Vark <wardi@mail.ru>
2020-10-08 22:19:58 +0700

    закончил реализацию функционала страницы Edit

--- RazorPagesGeneral/Pages/Employees/Edit.cshtml.cs
+++ RazorPagesGeneral/Pages/Employees/Edit.cshtml.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -13,15 +15,20 @@ namespace RazorPagesGeneral.Pages.Employees
     public class EditModel : PageModel
     {
         private readonly IEmployeeRepository _employeeRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public EditModel(IEmployeeRepository employeeRepository, IWebHostEnvironment webHostEnvironment)
+        {
+            _employeeRepository = employeeRepository;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [BindProperty]
         public Employee Employee { get; set; }
-        
+
         [BindProperty]
         public IFormFile Photo { get; set; }
 
-        public EditModel(IEmployeeRepository employeeRepository)
-        {
-            _employeeRepository = employeeRepository;
-        }
+        
 
       
 
@@ -35,8 +42,35 @@ namespace RazorPagesGeneral.Pages.Employees
 
         public IActionResult OnPost(Employee employee)
         {
+            if(Photo!= null)
+            {
+                if(employee.PhotoPath != null)
+                {
+                    string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "images", employee.PhotoPath);
+                    System.IO.File.Delete(filePath);
+
+                }
+
+                Employee.PhotoPath = ProcessUploadFile();
+            }
             Employee = _employeeRepository.Update(employee);
-            return RedirectToPage("/Employees");
+            return RedirectToPage("/Employees/Index");
+        }
+
+        private string ProcessUploadFile()
+        {
+            string uniqueFileName = null;
+            if(Photo != null)
+            {
+                string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath,"images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + Photo.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    Photo.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
         }
     }
 }
